99fba41bc97719fe40524aab976d016cd89b7890
Marcel Cornett <marcornett@gmail.com>
2020-10-10 15:21:08 -0400

    Header/Navbar containing the logo, links, and basket.

---
+++ src/Header.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import './Header.css'
+
+function Header() {
+    return (
+        <nav className="header">
+            {/* logo on left */}
+            <Link to='/'>
+                <img
+                    className="header__logo"
+                    src="https://onlinebusinessmanager.com/wp-content/uploads/2018/09/white-amazon-logo-png-6.png"
+                    alt=""
+                />
+            </Link>
+            {/* search box */}
+            <div className="header__search">
+                <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+            {/* 3 links */}
+            <div className="header__nav">
+                {/* 1st link */}
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Hello Marcel,</span>
+                        <span className="header__optionLineTwo">Sign in</span>
+                    </div>
+                </Link>
+                {/* 2nd link */}
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Orders</span>
+                    </div>
+                </Link>
+                {/* 3rd link */}
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
+                </Link>
+                {/* basket icon with number */}
+                <Link to="/checkout" className="header__link">
+                    <div className="header__optionBasket">
+                        {/* shopping basket icon */}
+                        <ShoppingBasketIcon />
+                        {/* number of items in basket */}
+                        <span className="header__optionLineTwo header__basketCount">0</span>
+                    </div>
+                </Link>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Header
