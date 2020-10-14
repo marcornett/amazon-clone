bf041435b8925c27cdccf40322fee232d6f6d7f0
Marcel Cornett <marcornett@gmail.com>
2020-10-14 09:46:04 -0400

    Mapping over basket items in state and return Checkout Product component with props.

---
+++ src/Checkout.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useStateValue } from './StateProvider'
+import CheckoutProduct from './CheckoutProduct'
+import Subtotal from './Subtotal'
+import './Checkout.css'
+
+function Checkout() {
+    const [state, dispatch] = useStateValue()
+
+    return (
+        <div className="checkout">
+            <div className="checkout__left">
+                <img
+                    className="checkout__ad"
+                    src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+                    alt=""
+                />
+                {state.basket?.length === 0 ? (
+                    <div>
+                        <h2 className="checkout__title">Your Amazon Cart is empty.y</h2>
+                        <p>You have no items in your basket. To buy one or more items, click "Add to basket" next to the item.</p>
+                    </div>
+                ) : (
+                        <div>
+                            <h2 className="checkout__title">Your Shopping Basket</h2>
+                            {/* list all checkout items */}
+                            {state.basket?.map((item, i) => (
+                                < CheckoutProduct
+                                    key={i}
+                                    id={item.id}
+                                    title={item.title}
+                                    price={item.price}
+                                    rating={item.rating}
+                                    image={item.image}
+                                />
+
+                            ))}
+                        </div>
+                    )}
+            </div>
+
+        </div>
+    )
+}
+
+export default Checkout
