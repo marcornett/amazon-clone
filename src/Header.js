55e400c4d1b04e34d842a3de722e73f9947464df
Marcel Cornett <marcornett@gmail.com>
2020-10-22 09:56:37 -0400

    Added bar under navigation.

--- src/Header.js
+++ src/Header.js
@@ -14,49 +14,52 @@ function Header() {
         }
     }
     return (
-        <nav className="header">
-            <Link to='/'>
-                <img
-                    className="header__logo"
-                    src="https://onlinebusinessmanager.com/wp-content/uploads/2018/09/white-amazon-logo-png-6.png"
-                    alt=""
-                />
-            </Link>
-            <div className="header__search">
-                <input type="text" className="header__searchInput" />
-                <SearchIcon className="header__searchIcon" />
-            </div>
-            <div className="header__nav">
-                <Link to={!state.user ? "/login" : ""} className="header__link">
-                    <div onClick={login} className="header__option">
-                        <span className="header__optionLineOne">Hello {state.user && state.user.email},</span>
-                        <span className="header__optionLineTwo">{
-                            state.user ? 'Sign out' : 'Sign in'
-                        }</span>
-                    </div>
+        <>
+            <nav className="header">
+                <Link to='/'>
+                    <img
+                        className="header__logo"
+                        src="https://onlinebusinessmanager.com/wp-content/uploads/2018/09/white-amazon-logo-png-6.png"
+                        alt=""
+                    />
                 </Link>
-                <Link to="/login" className="header__link">
-                    <div className="header__option">
-                        <span className="header__optionLineOne">Returns</span>
-                        <span className="header__optionLineTwo">& Orders</span>
-                    </div>
-                </Link>
-                <Link to="/login" className="header__link">
-                    <div className="header__option">
-                        <span className="header__optionLineOne">Your</span>
-                        <span className="header__optionLineTwo">Prime</span>
-                    </div>
-                </Link>
-                <Link to="/checkout" className="header__link">
-                    <div className="header__optionBasket">
-                        <ShoppingBasketIcon />
-                        {/* Optional chaining using ?  https://dev.to/aumayeung/how-to-use-the-optional-chaining-operator-in-your-react-app-right-now-1ocj */}
-                        <span className="header__optionLineTwo header__basketCount">{state.basket?.length}</span>
-                    </div>
-                </Link>
-            </div>
+                <div className="header__search">
+                    <input type="text" className="header__searchInput" />
+                    <SearchIcon className="header__searchIcon" />
+                </div>
+                <div className="header__nav">
+                    <Link to={!state.user ? "/login" : ""} className="header__link">
+                        <div onClick={login} className="header__option">
+                            <span className="header__optionLineOne">Hello {state.user && state.user.email},</span>
+                            <span className="header__optionLineTwo">{
+                                state.user ? 'Sign out' : 'Sign in'
+                            }</span>
+                        </div>
+                    </Link>
+                    <Link to="/login" className="header__link">
+                        <div className="header__option">
+                            <span className="header__optionLineOne">Returns</span>
+                            <span className="header__optionLineTwo">& Orders</span>
+                        </div>
+                    </Link>
+                    <Link to="/login" className="header__link">
+                        <div className="header__option">
+                            <span className="header__optionLineOne">Your</span>
+                            <span className="header__optionLineTwo">Prime</span>
+                        </div>
+                    </Link>
+                    <Link to="/checkout" className="header__link">
+                        <div className="header__optionBasket">
+                            <ShoppingBasketIcon />
+                            {/* Optional chaining using ?  https://dev.to/aumayeung/how-to-use-the-optional-chaining-operator-in-your-react-app-right-now-1ocj */}
+                            <span className="header__optionLineTwo header__basketCount">{state.basket?.length}</span>
+                        </div>
+                    </Link>
+                </div>
 
-        </nav>
+            </nav>
+            <div className="header__linkbar"></div>
+        </>
     )
 }
 
