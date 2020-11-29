d42ae73a930516ddf8f3e2db55d40ae265d237e5
Marcel Cornett <marcornett@gmail.com>
2020-11-29 18:34:19 -0500

    Add base url from Firebase, express endpoint.

--- src/Components/Axios/Axios.js
+++ src/Components/Axios/Axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: '...'
+    baseURL: 'http://localhost:5001/clone-1a782/us-central1/api',
+    // headers: 'pk_test_51HsttbDX941wjKlxV86OIIIlwYu6qQKt4gBoPjfRSocVAGquOq1SSkhZLmqgRxZq05CLrpTJVigqPMupYYdSrTeq00aFKEn3g5',
 })
 
 export default instance
